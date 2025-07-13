9f3c502dc57fbc92bfaf072b376c590a281c5772
Jailsonrib <131281208+Jailsonrib@users.noreply.github.com>
2025-07-12 21:56:26 -0300

    adicionei uma subclasse na classe User() para modelar um user admin

--- classes/classeUsers.py
+++ classes/classeUsers.py
@@ -5,7 +5,19 @@ class User:
         self.age = age
     def great_user(self):
         full_name = f" Name: {self.name} last_name: {self.last_name} age: {self.age}".title()
-        return full_name
+        print(full_name)
+class Admin(User):
 
-user1 = User("jailson",'Ribeiro', 31)
-print(user1.great_user())
+    def __init__(self, name, last_name, age):
+
+        super().__init__(name, last_name, age)
+
+    def great_admin(self):
+       print( f" Sir {self.name} welcome to the office")
+
+
+
+
+user1 = Admin("jailson",'Ribeiro', 31)
+user1.great_user()
+user1.great_admin()
